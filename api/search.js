950cb7baf887e7830c7470d6232b5d47a8123d09
Libraryipo <library@ipo.co.il>
2025-09-14 13:15:34 +0300

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // Enable CORS for Monday.com
+  // CORS Headers for Monday.com
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
